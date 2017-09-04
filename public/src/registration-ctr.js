c62dc4ce5790e18b2ea408d20445d5e44c579054
emiliano123% <emilianoinsfran@gmail.com>
2017-09-04 04:41:24 -0300

    registro de usuario tab1

---
+++ public/src/registration-ctr.js
@@ -0,0 +1,7 @@
+angular.module("main").controller("registrationCtrl",["$scope","$rootScope",function($scope,$rootScope){
+    $scope.userRegistration={
+        provinces : "Provincia"
+    }
+ 
+
+}])
